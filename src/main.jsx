298286532c83d02cb45ba83bc41ace4a9d457dee
luancristian09c-dev <luancristian09c@gmail.com>
2026-08-25 17:49:02 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import UnikeAdmin from "./src/UnikeAdmin.jsx";
+import UnikeAdmin from "./UnikeAdmin.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
